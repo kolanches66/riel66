9e4f393ff13d561264d29f242309a8842ae3fa26
Maksim Ponomarev <kolanches66@gmail.com>
2019-06-01 13:01:51 +0500

    добавил футер

--- wp-content/themes/themeriel66/footer.php
+++ wp-content/themes/themeriel66/footer.php
@@ -1,30 +1,29 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+    <footer class="footer">
+        <div class="footer__copyright">© <?php bloginfo('name'); ?>, <?=date('Y');?></div>
+        <div class="footer__contacts">
+            <div class="footer__contact">
+                <span class="footer__icon"><i class="fas fa-phone"></i></span>
+                <span><a class="footer__link" href="tel:+79028723894">+7 902 87 23 894</a></span>
+            </div>
+            <div class="footer__contact">
+                <span class="footer__icon"><i class="fas fa-envelope"></i></span>
+                <span><a class="footer__link" href="mailto:ponomareva@riel66.ru">ponomareva@riel66.ru</a></span>
+            </div>
+        </div>
+    </footer>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+    <?php wp_footer(); ?>
 
-			</footer>
-			<!-- /footer -->
+    <!-- analytics -->
+    <script>
+        (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+            (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+            l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+        ga('send', 'pageview');
+    </script>
 
-		</div>
-		<!-- /wrapper -->
+</body>
 
-		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
-
-	</body>
 </html>
