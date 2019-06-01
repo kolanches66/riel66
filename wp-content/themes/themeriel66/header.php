689402579935b958d6c27f87cb3dc08bbcea6211
Maksim Ponomarev <kolanches66@gmail.com>
2019-06-01 12:31:49 +0500

    сделал хэдэр;

--- wp-content/themes/themeriel66/header.php
+++ wp-content/themes/themeriel66/header.php
@@ -1,50 +1,76 @@
-<!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<?php wp_head(); ?>
-		<script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+
+    <?php wp_head(); ?>
+    <script>
         // conditionizr.com
         // configure environment tests
         conditionizr.config({
             assets: '<?php echo get_template_directory_uri(); ?>',
             tests: {}
         });
-        </script>
+    </script>
 
-	</head>
-	<body <?php body_class(); ?>>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=get_template_directory_uri();?>/css/normalize.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri();?>/css/styles.css">
+</head>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+<body>
+<section class="welcome">
+    <h1 class="welcome__experience">12 лет</h1>
+    <h2 class="welcome__work">работы в недвижимости</h2>
+    <div class="welcome__slogan">риэлтор, которого вы искали</div>
+    <div class="welcome__avatar">
+        <img class="welcome__avatar__image" src="<?=get_template_directory_uri();?>/img/ponomareva-marina.png" alt="">
+    </div>
+</section>
 
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
+<header class="sect header">
+    <div class="header__container">
+        <div class="header__whoami">
+            <div class="header__whoami__lastname">Пономарева</div>
+            <div class="header__whoami__firstname">Марина</div>
+            <div class="header__whoami__description">агент по недвижимости</div>
+        </div>
+        <div class="header__contacts">
+            <div class="header__contacts__callme">Свяжитесь со мной:</div>
+            <div class="header__contacts__icons">
+                <div class="header__contacts__icons__icon">
+                    <a href="tel:+79028723894" class="header__contacts__icons__iconlink">
+                        <i class="fas fa-phone"></i>
+                    </a>
+                </div>
+                <div class="header__contacts__icons__icon">
+                    <a href="mailto:ponomarev@riel66.ru" class="header__contacts__icons__iconlink">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                </div>
+                <div class="header__contacts__icons__icon">
+                    <a class="header__contacts__icons__iconlink">
+                        <i class="fab fa-telegram"></i>
+                    </a>
+                </div>
+                <div class="header__contacts__icons__icon">
+                    <a href="https://wa.me/79028723894" class="header__contacts__icons__iconlink">
+                        <i class="fab fa-whatsapp"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="header__divider">
+</header>
+<body <?php body_class(); ?>>
